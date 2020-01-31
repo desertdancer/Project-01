2aaff4721083ffc012c469da83c78fca88503253
desertdancer <steviepop@icloud.com>
2020-01-31 00:10:12 -0700

    solving search issue

--- assets/javascript/prjct1.js
+++ assets/javascript/prjct1.js
@@ -41,9 +41,6 @@ function buildQueryURL() {
     .val()
     .trim();
 
-
-
-
   // Logging the URL so we have access to it for troubleshooting
   console.log("---------------\nURL: " + queryURL + "\n---------------");
   console.log(queryURL + $.param(queryParams));
@@ -51,15 +48,15 @@ function buildQueryURL() {
 }
 
 /**
- * takes API data (JSON/object) and turns it into elements on the page
- * @param {object} GRData - object containing NYT API data
+ * takes API data (JSON/object) and turns it into elements on page
+ * @param {object} GRData - object containing Google API data
  */
 function updatePage(GRData) {
   console.log("hurray");
 
   console.log(GRData);
-  // Get from the form the number of results to display
-  // API doesn't have a "limit" parameter, so we have to do this ourselves
+  // Get number of results to display from the form 
+  // IF API doesn't hv "limit" parameter, we hv to do ourself
   var numArticles = $("#article-count").val();
 
   // Log the GRData to console, where it will show up as an object
@@ -68,7 +65,7 @@ function updatePage(GRData) {
   console.log($(x).find("title")[0])
   console.log("------------------------------------");
 
-  // Loop through and build elements for the defined number of articles
+  // Loop through and build elements for defined number of articles
   for (var i = 0; i < numArticles; i++) {
     // Get specific article info for current index
     var article = $(GRData).find("title")[i];
@@ -76,7 +73,7 @@ function updatePage(GRData) {
     // Increase the articleCount (track article # - starting at 1)
     var articleCount = i + 1;
 
-    // Create the  list group to contain the articles and add the article content for each
+    // Create the  list group to contain the articles(books) and add the article content for each
     var $articleList = $("<ul>");
     $articleList.addClass("list-group");
 
@@ -194,19 +191,194 @@ function googleBooks() {
       })
     })
 }
-function NYTBooks() {
-  var url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=DhrqyjaAn0mxLFOdpPxByhBW2Sxa6XDQ"
-  $.get(url).then(function (responseData) {
-    console.log(responseData)
 
-    var books = responseData.results.books
-    console.log(books)
-    for (var i = 0; i < books.length; i++) {
+// BEGINNING OF NYTBOOKS CODE ISABEL HELPED ME WITH
+// NOTE - NEED NAV TO GO BETWEEN SEARCH SCREEN AND SAVE SCREEN
+// NEED WAY BETTER LAYOUT
+//Need separate SEARCH button?
+//Search results need to come into the card BELOW the Search Results headline
+//Best Seller list needs to wait to be searched...right now it automatically comes after the Books/description
+//Different results field also?
+//Wamt Best Sellers list to be separate request an either/or  books or reviews
+
+// function NYTBooks() {
+  
+  // var url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=DhrqyjaAn0mxLFOdpPxByhBW2Sxa6XDQ"
+  // $.get(url).then(function (responseData) {
+    // console.log(responseData)
+
+    // var books = responseData.results.books
+    // console.log(books)
+    // for (var i = 0; i < books.length; i++) {
 
 
-      $("#NYTBooks").append(`<p>${books[i].title} - ${books[i].author}</p> <br>`)
-    }
+      // $("#NYTBooks").append(`<p>${books[i].title} - ${books[i].author}</p> <br>`)
+    // }
 
-  })
-}
-NYTBooks()
+  // })
+// }
+// NYTBooks()
+// END OF NYTBOOKS CODE THAT ISABEL HELPED ME WITH ON WED
+
+
+----------------------------
+
+// I PASTED ALL THE ORIGINAL CODE FROM THE ORIGINAL NYT ACTIVITY TO SEE IF I COULD MODIFY IT TO WORK WITH THE 
+// THE NYTBOOKS API AND MY KEY...WHEN I DID THIS, NONE OF THE DATA FROM THE GOOGLE BOOKS APP IS PULLING, SO I AM COMMENTING ALL BELOW OUT
+
+
+// pulls information from the form and build the query URL
+// * @returns {string} URL for NYT API based on form inputs
+
+// */
+// function buildQueryURL() {
+
+ // queryURL is the url we'll use to query the API
+//  var queryURL = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=DhrqyjaAn0mxLFOdpPxByhBW2Sxa6XDQ";
+
+ // Begin building an object to contain our API call's query parameters
+ // Set the API key
+//  var queryParams = { "api-key": "DhrqyjaAn0mxLFOdpPxByhBW2Sxa6XDQ"};
+
+
+ // Grab text the user typed into the search input, add to the queryParams object
+//  queryParams.q = $("#search-term")
+  //  .val()
+  //  .trim();
+
+ // If the user provides a startYear, include it in the queryParams object
+//  var startYear = $("#start-year")
+  //  .val()
+  //  .trim();
+
+//  if (parseInt(startYear)) {
+  //  queryParams.begin_date = startYear + "0101";
+//  }
+
+ // If the user provides an endYear, include it in the queryParams object
+//  var endYear = $("#end-year")
+  //  .val()
+  //  .trim();
+
+//  if (parseInt(endYear)) {
+  //  queryParams.end_date = endYear + "0101";
+//  }
+
+ // Logging the URL so we have access to it for troubleshooting
+//  console.log("---------------\nURL: " + queryURL + "\n---------------");
+//  console.log(queryURL + $.param(queryParams));
+//  return queryURL + $.param(queryParams);
+// }
+
+
+/**
+* takes API data (JSON/object) and turns it into elements on the page
+//  @param {object} NYTData - object containing NYT API data
+*/
+// function updatePage(NYTData) {
+
+ // Get from the form the number of results to display
+ // API doesn't have a "limit" parameter, so we have to do this ourselves
+
+//  var numArticles = $("#article-count").val();
+
+ // Log the NYTData to console, where it will show up as an object
+//  console.log(NYTData);
+//  console.log("------------------------------------");
+
+ // Loop through and build elements for the defined number of articles
+//  for (var i = 0; i < numArticles; i++) {
+
+   // Get specific article info for current index
+  //  var article = NYTData.response.docs[i];
+// 
+   // Increase the articleCount (track article # - starting at 1)
+  //  var articleCount = i + 1;
+
+   // Create the  list group to contain the articles and add the article content for each
+  //  var $articleList = $("<ul>");
+  //  $articleList.addClass("list-group");
+
+   // Add the newly created element to the DOM
+  //  $("#article-section").append($articleList);
+
+   // If the article has a headline, log and append to $articleList
+  //  var headline = article.headline;
+  //  var $articleListItem = $("<li class='list-group-item articleHeadline'>");
+
+  //  if (headline && headline.main) {
+    //  console.log(headline.main);
+    //  $articleListItem.append(
+      //  "<span class='label label-primary'>" +
+        //  articleCount +
+        //  "</span>" +
+        //  "<strong> " +
+        //  headline.main +
+        //  "</strong>"
+    //  );
+  //  }
+
+   // If the article has a byline, log and append to $articleList
+  //  var byline = article.byline;
+
+  //  if (byline && byline.original) {
+    //  console.log(byline.original);
+    //  $articleListItem.append("<h5>" + byline.original + "</h5>");
+  //  }
+
+   // Log section, and append to document if exists
+  //  var section = article.section_name;
+  //  console.log(article.section_name);
+  //  if (section) {
+    //  $articleListItem.append("<h5>Section: " + section + "</h5>");
+  //  }
+
+   // Log published date, and append to document if exists
+  //  var pubDate = article.pub_date;
+  //  console.log(article.pub_date);
+  //  if (pubDate) {
+    //  $articleListItem.append("<h5>" + article.pub_date + "</h5>");
+  //  }
+
+   // Append and log url
+  //  $articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
+  //  console.log(article.web_url);
+
+   // Append the article
+  //  $articleList.append($articleListItem);
+//  }
+// }
+
+// Function to empty out the articles
+// function clear() {
+//  $("#article-section").empty();
+// }
+
+// CLICK HANDLERS
+// ==========================================================
+
+// .on("click") function associated with the Search Button
+// $("#run-search").on("click", function(event) {
+
+ // This line allows us to take advantage of the HTML "submit" property
+ // This way we can hit enter on the keyboard and it registers the search
+
+ // (in addition to clicks). Prevents the page from reloading on form submit.
+//  event.preventDefault();
+
+ // Empty the region associated with the articles
+//  clear();
+
+ // Build the query URL for the ajax request to the NYT API
+//  var queryURL = buildQueryURL();
+
+ // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+ // The data then gets passed as an argument to the updatePage function
+//  $.ajax({
+  //  url: queryURL,
+  //  method: "GET"
+//  }).then(updatePage);
+// });
+
+//  .on("click") function associated with the clear button
+// $("#clear-all").on("click", clear);
